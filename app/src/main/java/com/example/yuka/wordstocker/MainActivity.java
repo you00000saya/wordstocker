4e15e27edfa2633096d142985e8c92d51574af8d
Yuka Hara <yukahasegawa0815@gmail.com>
2019-01-25 22:52:44 +0900

    Added WebView Activity.

--- app/src/main/java/com/example/yuka/wordstocker/MainActivity.java
+++ app/src/main/java/com/example/yuka/wordstocker/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.yuka.wordstocker;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonSearch;
+    private TextInputEditText textSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSearch = findViewById(R.id.button_search);
+        textSearch = findViewById(R.id.text_search);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSearch();
+
+            }
+        });
+
+    }
+
+    public void startSearch(){
+        Intent intent = new Intent(this, WebViewActivity.class);
+//        intent.putExtra(Intent.EXTRA_EMAIL, recipientArray);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/yuka/wordstocker/WebViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.yuka.wordstocker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+    }
+}
